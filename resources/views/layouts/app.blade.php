baa9cdc1cc3b9184278b132ae3e0b3613993f64b
Nurbismii <nurbismi74@gmail.com>
2025-05-01 15:31:14 +0800

    test: fix title bar name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Grow HR</title>
+    <title>Menu | HR Grow</title>
 
     <meta name="description" content="" />
 
